66cc50355431ca76b22a38dd0c8d24acf63db8d9
leolopezOsornio <leol83377@gmail.com>
2026-05-05 16:58:13 -0600

    feat(home): add discover events grid section

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import styles from "./home.module.css";
 import { Hero } from "../../widgets/hero/ui/hero";
 import { EventList } from "../../widgets/events/ui/EventList";
+import { DiscoverGrid } from "../../widgets/discover/ui/DiscoverGrid";
 
 export const HomePage = () => {
     return (
         <main className={styles.main}>
             <Hero />
+            <DiscoverGrid />
             <EventList />
         </main>
     );
